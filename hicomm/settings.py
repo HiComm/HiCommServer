fa774c6c882fb4c627466f8b835bc78d0171ebd6
T.M <tm3.14ex4159pc@gmail.com>
2021-05-03 11:47:27 +0900

    change settings adapted to local (sqlite)

--- hicomm/settings.py
+++ hicomm/settings.py
@@ -75,7 +75,7 @@ WSGI_APPLICATION = 'hicomm.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-'''
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -97,6 +97,7 @@ DATABASES = {
         },
     }
 }
+'''
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
