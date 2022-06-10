34a848b2f54bc2b9600557f4823eb5aad0517bac
tm <t.matsutake@hioki.co.jp>
2022-06-10 16:21:15 +0900

    add initialize script

--- initialize.py
+++ initialize.py
@@ -1,5 +1,8 @@
 from django.core.management.utils import get_random_secret_key
 
 secret_key = get_random_secret_key()
+debug = (input("Debug Environment? [y/n]") != "n")
 
-DEBUG = (input("NOT Production Environment? [y/n]") == "y")
+with open("./hicomm/secret_settings.py", "w") as f:    
+    f.write(f"DEBUG = {debug}\n")
+
