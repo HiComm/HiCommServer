4e882adf0e23a621979e3336848a885e9ad65f0e
tm <t.matsutake@hioki.co.jp>
2022-06-10 16:24:22 +0900

    modify

--- initialize.py
+++ initialize.py
@@ -3,6 +3,7 @@ from django.core.management.utils import get_random_secret_key
 secret_key = get_random_secret_key()
 debug = (input("Debug Environment? [y/n]") != "n")
 
-with open("./hicomm/secret_settings.py", "w") as f:    
+with open("./hicomm/secret_settings.py", "w") as f:
+    f.write(f"SECRET_KEY = {secret_key}\n")
     f.write(f"DEBUG = {debug}\n")
 
